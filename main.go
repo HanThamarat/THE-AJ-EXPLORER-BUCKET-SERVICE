5d537da7cd2941ad581b600e99366cf29d430657
HanThamarat <HanThamarat@gmail.com>
2025-11-08 22:58:54 +0700

    fix currrent host

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 // @contact.email technexify@outlook.com
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host bucket-app.theajexplorer.com
+// @host localhost:3004
 // @BasePath /api
 // @schemes http https
 // @securityDefinitions.apikey BearerAuth
